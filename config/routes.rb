6d8906c7e2d5990de349936d3b80feb263e7fc4a
sebastian fuenza rails <sebastian.fuenza@gmail.com>
2017-12-20 11:10:42 -0300

    navbar y container

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :products
   get 'pages/home'
 
   get 'pages/about'
+
+  root 'pages#home'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
